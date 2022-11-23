c79cc2f6d64cf99141ed5e363aa1e29260191235
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2022-11-23 09:45:42 +0330

    Initial commit

---
+++ code.cpp
@@ -0,0 +1,4 @@
+//
+// Created by mokazemi-93 on 11/23/22.
+//
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mokazemi-93 on 11/23/22.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello!" << endl;
+}
